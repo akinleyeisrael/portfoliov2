ac06f6ae16545175a1583f1b4e13b5943b07626d
israel <akinleyeisrael12@gmail.com>
2024-01-25 04:18:19 -0500

    started second iteration of my portfolio with contentful

---
+++ lib/actions.ts
@@ -0,0 +1,13 @@
+"use server"
+
+import { createClient } from "contentful";
+
+const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID as string,
+    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN as string,
+});
+
+export async function getPorfolioEntries() {
+    const entries = await client.getEntries({ content_type: "portfoliov2" });
+    return entries;
+}
