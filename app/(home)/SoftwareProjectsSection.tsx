50bb189e8795625bda7f8e34a1e696310fc3673c
israel <akinleyeisrael12@gmail.com>
2024-03-29 20:45:37 -0400

    added smoth scroll

--- app/(home)/SoftwareProjectsSection.tsx
+++ app/(home)/SoftwareProjectsSection.tsx
@@ -44,7 +44,7 @@ const SoftwareProjects = async () => {
           } = portfolio.fields;
           return (
             <div key={idx}>
-              <SlideUp offset="-300px 0px -300px 0px">
+              {/* <SlideUp offset="-300px 0px -300px 0px"> */}
                 <div className="flex flex-col animate-slideUpCubiBezier animation-delay-2 md:flex-row md:space-x-12">
                   <div className=" md:w-1/2 shadow-2xl">
                     <Link href={weblink as string}>
@@ -92,7 +92,7 @@ const SoftwareProjects = async () => {
                     </div>
                   </div>
                 </div>
-              </SlideUp>
+              {/* </SlideUp> */}
             </div>
           );
         })}
