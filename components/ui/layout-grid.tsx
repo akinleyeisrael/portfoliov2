e132ce5e2b405ee9180812d211a4a179cdcfdc0a
israel <akinleyeisrael12@gmail.com>
2024-04-01 19:28:01 -0400

    change layout grid to 2

--- components/ui/layout-grid.tsx
+++ components/ui/layout-grid.tsx
@@ -26,7 +26,7 @@ export const LayoutGrid = ({ cards }: { cards: Card[] }) => {
     };
 
     return (
-        <div className="w-full h-full grid grid-cols-1 md:grid-cols-3  max-w-7xl mx-auto gap-4 ">
+        <div className="w-full h-full grid grid-cols-2 md:grid-cols-3  max-w-7xl mx-auto gap-4 ">
             {cards.map((card, i) => (
                 <div key={i} className={cn(card.className, "")}>
                     <motion.div
