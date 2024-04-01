2957ffd07773069b9878e597ff479989d37e8aae
israel <akinleyeisrael12@gmail.com>
2024-04-01 18:37:57 -0400

    finished portfolio

--- app/(home)/Footer.tsx
+++ app/(home)/Footer.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export const Footer = () => {
     return (
-        <div className=" text-center pb-10 text-sm text-slate-400 space-y-4">
+        <div className=" text-center pb-10 text-sm text-slate-400 space-y-2">
             <p> Built with <span className="font-semibold text-slate-200">Next.js</span> and <span className="font-semibold text-slate-200">Tailwind CSS</span>, deployed with <span className="font-semibold">Vercel</span></p>
             <p>Inspired by <Link href="https://brittanychiang.com/" target='_blank' className="hover:cursor-pointer hover:underline hover:text-lime-300  font-medium text-slate-200">BrittanyChiang</Link></p>
         </div>
