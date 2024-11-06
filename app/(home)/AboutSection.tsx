8092a4b715f0e8a923bec0e4d42a29249f58659b
AKINOLA <41457034+akinleyeisrael@users.noreply.github.com>
2024-11-06 17:03:50 -0500

    Update AboutSection.tsx

--- app/(home)/AboutSection.tsx
+++ app/(home)/AboutSection.tsx
@@ -1,137 +1,130 @@
 /* eslint-disable react/no-unescaped-entities */
-import { LayoutGrid } from '@/components/ui/layout-grid';
-import { TypewriterEffect } from '@/components/ui/typewriter-effect';
-import { SocialMedia } from './socialMedias';
+import { LayoutGrid } from "@/components/ui/layout-grid";
+import { TypewriterEffect } from "@/components/ui/typewriter-effect";
+import { SocialMedia } from "./socialMedias";
 
 const words = [
-    {
-        text: "hey",
-    },
-    {
-        text: "I'am",
-    },
-    {
-        text: "Akinola",
-        className: "text-lime-300"
-    },
-    {
-        text: "Akinleye.",
-        className: "text-lime-300"
-    },
+  {
+    text: "hey",
+  },
+  {
+    text: "I'am",
+  },
+  {
+    text: "Akinola",
+    className: "text-lime-300",
+  },
+  {
+    text: "Akinleye.",
+    className: "text-lime-300",
+  },
 ];
 const AboutSection = () => {
-    return (
-        <section id='about' className='container'>
-            <div>
-                <div className='py-4'>
-                    <h1 className='text-5xl flex flex-col text-lime-400 font-bold text'>Frontend <span>Developer.</span></h1>
-                    <div className='pt-2'>
-                        <TypewriterEffect words={words} />
-                    </div>
-                </div>
-                <p className='text-slate-400 tracking-normal leading-loose'>
-                    I am a passionate <span className='font-bold'>Frontend Developer </span>with a relentless drive for <span className='font-bold'>innovation</span> and <span className='font-bold'>problem-solving</span>. Graduating with a degree in <span className='font-bold'>Computer Programming</span> I've embarked on a journey to weave intricate webs of code into transformative digital experiences.
-                </p>
-                <div className='block md:hidden lg:hidden'>
-                    <SocialMedia />
-                </div>
-            </div>
-            <div className="h-screen py-10 w-full">
-                <LayoutGrid cards={cards} />
-            </div>
-            <div className="pt-2">
-                <p className='text-slate-400 tracking-normal leading-loose'>
-                    Beyond the world of programming,I am also a skilled <span className='font-bold'>cinematographer,</span> proficient in capturing captivating <span>videos</span> with a keen eye for <span className='font-bold'>visual storytellin</span>g.
-                </p>
-            </div>
-        </section>
-    )
-}
-
+  return (
+    <section id="about" className="container">
+      <div>
+        <div className="py-4">
+          <h1 className="text-5xl flex flex-col text-lime-400 font-bold text">
+            Frontend <span>Developer.</span>
+          </h1>
+          <div className="pt-2">
+            <TypewriterEffect words={words} />
+          </div>
+        </div>
+        <p className="text-slate-400 tracking-normal leading-loose">
+          I am a passionate{" "}
+          <span className="font-bold">Frontend Developer </span>with a
+          relentless drive for <span className="font-bold">innovation</span> and{" "}
+          <span className="font-bold">problem-solving</span>. Graduating with a
+          degree in <span className="font-bold">Computer Programming</span> I've
+          embarked on a journey to weave intricate webs of code into
+          transformative digital experiences.
+        </p>
+        <div className="block md:hidden lg:hidden">
+          <SocialMedia />
+        </div>
+      </div>
+      <div className="h-screen py-10 w-full">
+        <LayoutGrid cards={cards} />
+      </div>
+      <div className="pt-2">
+        <p className="text-slate-400 tracking-normal leading-loose">
+          Beyond the world of programming,I am also a skilled{" "}
+          <span className="font-bold">cinematographer,</span> proficient in
+          capturing captivating <span>videos</span> with a keen eye for{" "}
+          <span className="font-bold">visual storytellin</span>g.
+        </p>
+      </div>
+    </section>
+  );
+};
 
 const SkeletonOne = () => {
-    return (
-        <div>
-            <p className="font-bold text-4xl text-white">House in the woods</p>
-            <p className="font-normal text-base text-white"></p>
-            <p className="font-normal text-base my-4 max-w-lg text-neutral-200">
-                A serene and tranquil retreat, this house in the woods offers a peaceful
-                escape from the hustle and bustle of city life.
-            </p>
-        </div>
-    );
+  return (
+    <div>
+      <p className="font-bold text-4xl text-white">House in the woods</p>
+      <p className="font-normal text-base text-white"></p>
+      
+    </div>
+  );
 };
 
 const SkeletonTwo = () => {
-    return (
-        <div>
-            <p className="font-bold text-4xl text-white">House above the clouds</p>
-            <p className="font-normal text-base text-white"></p>
-            <p className="font-normal text-base my-4 max-w-lg text-neutral-200">
-                Perched high above the world, this house offers breathtaking views and a
-                unique living experience. It&apos;s a place where the sky meets home,
-                and tranquility is a way of life.
-            </p>
-        </div>
-    );
+  return (
+    <div>
+      <p className="font-bold text-4xl text-white">House above the clouds</p>
+      <p className="font-normal text-base text-white"></p>
+    
+    </div>
+  );
 };
 const SkeletonThree = () => {
-    return (
-        <div>
-            <p className="font-bold text-4xl text-white">Greens all over</p>
-            <p className="font-normal text-base text-white"></p>
-            <p className="font-normal text-base my-4 max-w-lg text-neutral-200">
-                A house surrounded by greenery and nature&apos;s beauty. It&apos;s the
-                perfect place to relax, unwind, and enjoy life.
-            </p>
-        </div>
-    );
+  return (
+    <div>
+      <p className="font-bold text-4xl text-white">Greens all over</p>
+      <p className="font-normal text-base text-white"></p>
+     
+    </div>
+  );
 };
 const SkeletonFour = () => {
-    return (
-        <div>
-            <p className="font-bold text-4xl text-white">Rivers are serene</p>
-            <p className="font-normal text-base text-white"></p>
-            <p className="font-normal text-base my-4 max-w-lg text-neutral-200">
-                A house by the river is a place of peace and tranquility. It&apos;s the
-                perfect place to relax, unwind, and enjoy life.
-            </p>
-        </div>
-    );
+  return (
+    <div>
+      <p className="font-bold text-4xl text-white">Rivers are serene</p>
+      <p className="font-normal text-base text-white"></p>
+    
+    </div>
+  );
 };
 
 const cards = [
-    {
-        id: 1,
-        content: <SkeletonOne />,
-        className: "md:col-span-2",
-        thumbnail:
-            "https://images.unsplash.com/photo-1476231682828-37e571bc172f?q=80&w=3474&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
-    {
-        id: 2,
-        content: <SkeletonTwo />,
-        className: "col-span-1",
-        thumbnail:
-            "https://images.unsplash.com/photo-1464457312035-3d7d0e0c058e?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
-    {
-        id: 3,
-        content: <SkeletonThree />,
-        className: "col-span-1",
-        thumbnail:
-            "https://images.unsplash.com/photo-1588880331179-bc9b93a8cb5e?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
-    {
-        id: 4,
-        content: <SkeletonFour />,
-        className: "md:col-span-2",
-        thumbnail:
-            "https://images.unsplash.com/photo-1475070929565-c985b496cb9f?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
+  {
+    id: 1,
+    content: <SkeletonOne />,
+    className: "md:col-span-2",
+    thumbnail:
+      "https://unsplash.com/photos/woman-in-black-shirt-sitting-beside-black-flat-screen-computer-monitor-Im_cQ6hQo10",
+  },
+  {
+    id: 2,
+    content: <SkeletonTwo />,
+    className: "col-span-1",
+    thumbnail:
+      "https://unsplash.com/photos/a-close-up-of-a-sign-with-the-word-hello-hello-jxelyjTrWFg",
+  },
+  {
+    id: 3,
+    content: <SkeletonThree />,
+    className: "col-span-1",
+    thumbnail:
+      "https://unsplash.com/photos/black-flat-screen-computer-monitor-8qEB0fTe9Vw",
+  },
+  {
+    id: 4,
+    content: <SkeletonFour />,
+    className: "md:col-span-2",
+    thumbnail: "/akin.png",
+  },
 ];
 
-
-
-
-export default AboutSection
+export default AboutSection;
